54052ab875d5bd30b286dc02aa8b2cac2b64b919
rssr123 <77654144+rssr123@users.noreply.github.com>
2026-06-26 00:27:54 +0800

    Delete CRS directory

--- CRS/src/app/interfaces/icrspayment.ts
+++
@@ -1,9 +0,0 @@
-export interface ICRSPayment {
-    id: number;
-    description: string;
-    quantity: number;
-    amount: number;
-    tax: number;
-    discount: number;
-    grossAmount: number;
-}
